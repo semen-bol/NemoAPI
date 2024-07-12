7dc5272f7fdf04e0969508ed15c4a0ccbec56ef6
NemoDev <82635729+semen-bol@users.noreply.github.com>
2024-07-12 17:11:09 +0500

    Added example

--- api/debug/test.js
+++ api/debug/test.js
@@ -1,6 +1,6 @@
 const nemo = require("nemocm-api"); 
 
-const api = new nemo.api({token: "nemo.t.test", url: "http://localhost/method/"})
+const api = new nemo.api({token: ""})
 
 async function getInfo() {
     let request = await api.utils.getShortLink({link: "vk.com"})
